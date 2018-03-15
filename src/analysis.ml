94420066c0a584f172a8a01f8c0e7dd46592ccdc
Johan Myrsmeden <johan@myrsmeden.se>
2018-03-15 13:43:21 +0100

    Removing unused functions

--- src/analysis.ml
+++ src/analysis.ml
@@ -14,6 +14,13 @@ module StringMap = Map.Make (String)
 
 (* A function consists of a name and number of params *)
 type fd = FuncData of ustring * int
+type errormsg = 
+	| Name of ustring
+	| NumberOfParams of int 
+	| IsVoid of bool 
+	| Info of info
+	| Type of string
+	| Data of errormsg * errormsg
 
 (* Function to append two lists *)
 let append l1 l2 =
@@ -69,114 +76,6 @@ let rec exists_funcdata item lst =
 (* Check if item exists in env-part of map env *)		
 let exists_in_environment item env lst_name = 
 	exists item (StringMap.find lst_name env)
-
-(* Function to conduct a list of all variables in an ast *)
-let fetch_variables ast =
-	let rec traverse ast acc = 
-	  match ast with
-		(* Statements *)
-		 | TmDef(_,_,name,tm) -> traverse tm acc
-		 | TmWhile (_, tm_head, tm_body) -> traverse tm_head (traverse tm_body acc)
-		 | TmIf(_,_,tm2,tm3) -> append (traverse tm2 acc) (match tm3 with 
-		 	| Some(tm) -> traverse tm acc 
-		 	| None -> acc )
-		 | TmAssign(_,name,tm) -> traverse tm (name::acc)
-		 | TmRet(_,_) -> acc
-		(* Expressions *)
-		 | TmVar(_,_,name) -> name::acc
-		 | TmConst(_,_) -> acc
-		 | TmFunc(_,_,tm) -> traverse tm acc
-		 | TmCall(_,_,tmlist) -> reduce traverse tmlist acc
-		(* Other *)
-		 | TmScope(_,tmlist) -> reduce traverse tmlist acc
-	in traverse ast []
-
-(* Fetch all variables until new scope is reached *)
-let fetch_variables_scope ast =
-	let rec traverse ast acc = 
-	  match ast with
-		(* Statements *)
-		 | TmDef(_,isconst,name,tm) -> traverse tm (if isconst then acc else (name::acc))
-		 | TmWhile (_, tm_head, tm_body) -> traverse tm_head (traverse tm_body acc)
-		 | TmIf(_,_,tm2,tm3) -> append (traverse tm2 acc) (match tm3 with 
-		 	| Some(tm) -> traverse tm acc 
-		 	| None -> acc )
-		 | TmAssign(_,name,tm) -> traverse tm (name::acc)
-		 | TmRet(_,_) -> acc
-		(* Expressions *)
-		 | TmVar(_,_,name) -> name::acc
-		 | TmConst(_,_) -> acc
-		 | TmFunc(_,_,tm) -> traverse tm acc
-		 | TmCall(_,_,tmlist) -> reduce traverse tmlist acc
-		(* Other *)
-		 | TmScope(_,tmlist) -> acc
-	in traverse ast []
-
-(* Function to rename some variables, only to practice transformation of the AST *)
-let rename_variable ast =
-	let rec traverse ast =  
-	  match ast with
-		(* Statements *)
-		 | TmDef(fi,isconst,name,tm) -> (
-		 	match tm with 
-		 		| TmFunc(fi2, params, tm2) -> TmDef(fi, isconst, name, TmFunc(fi, params, traverse tm2))
-		 		| TmVar(fi2, isconst2, name2) -> TmDef(fi, isconst, name, TmVar(fi2, isconst2, us"vardef"))
-		 		| TmAssign(fi2,name,tm2) -> TmDef(fi, isconst, name, TmAssign(fi2, us"defas", traverse tm2))
-		 		| _ -> TmDef(fi, isconst, us"odeffad", traverse tm))
-		 | TmWhile (fi, tm_head, tm_body) -> TmWhile(fi, traverse tm_head, traverse tm_body)
-		 | TmIf(fi,tm1,tm2,tm3) -> TmIf(fi, traverse tm1, traverse tm2, 
-		 	(match tm3 with 
-		 		| Some(tm) -> Some(traverse tm)
-		 		| None -> tm3 ))
-		 | TmAssign(fi,name,tm) -> TmAssign(fi, us"varis", traverse tm)
-		 | TmRet(fi,tm) -> TmRet(fi, traverse tm)
-		(* Expressions *)
-		 | TmVar(fi,isconst,name) -> TmVar(fi, isconst, us"variabeln")
-		 | TmConst(fi,const) -> TmConst(fi, const)
-		 | TmFunc(fi,const,tm) -> TmFunc(fi, const, traverse tm)
-		 | TmCall(fi,tm,tmlist) -> TmCall(fi, tm, map traverse tmlist)
-		(* Other *)
-		 | TmScope(fi,tmlist) -> TmScope(fi, map traverse tmlist)
-	in traverse ast
-
-let rename_in_scope ast name_to_replace new_name = 
-	let rec traverse ast = 
-		match ast with 
-		(* Statements *)
-		 | TmDef(fi,isconst,name,tm) -> uprint_endline (us"Def av " ^. name); 
-		 	(match tm with 
-		 		| TmFunc(fi2, const, tm2) -> 
-		 		print_string "Def is func";
-		 		uprint_endline(pprint tm2);
-		 			if Ustring.equal name name_to_replace then
-		 				TmDef(fi, isconst, new_name, traverse tm)
-		 			else
-		 				TmDef(fi, isconst, name, traverse tm)
-		 		| _ -> TmDef(fi, isconst, name, traverse tm))
-		 | TmWhile (fi, tm_head, tm_body) -> TmWhile(fi, traverse tm_head, traverse tm_body)
-		 | TmIf(fi,tm1,tm2,tm3) -> TmIf(fi, traverse tm1, traverse tm2, 
-		 	(match tm3 with 
-		 		| Some(tm) -> Some(traverse tm)
-		 		| None -> tm3 ))
-		 | TmAssign(fi,name,tm) -> print_endline "Assign";
-		 	if Ustring.equal name name_to_replace then
-		 		TmAssign(fi, new_name, traverse tm)
-		 	else
-		 		TmAssign(fi, name, traverse tm)
-		 | TmRet(fi,tm) -> print_endline "Ret"; TmRet(fi, traverse tm)
-		(* Expressions *)
-		 | TmVar(fi,isconst,name) -> 
-		 	print_string "-Var";
-		 	if Ustring.equal name name_to_replace then
-		 		TmVar(fi, isconst, new_name)
-		 	else
-		 		TmVar(fi, isconst, name)
-		 | TmConst(fi,const) -> print_endline "Const"; TmConst(fi, const)
-		 | TmFunc(fi,params,tm) -> print_string "Params:"; print_list params; TmFunc(fi, params, traverse tm)
-		 | TmCall(fi,tm,tmlist) -> TmCall(fi, tm, map traverse tmlist)
-		(* Other *)
-		 | TmScope(fi,tmlist) -> print_endline "Scope"; TmScope(fi, map traverse tmlist)
-	in traverse ast
 
 (* Functions to handle environment 
    it is a map with lists, under the strings provided in lst_names
